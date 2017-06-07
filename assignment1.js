aaea24474f01157cce844428ed9bf85b20d68d5b
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-07 17:18:12 +0530

    author changed mistakes in file removed

--- assignment1.js
+++ assignment1.js
@@ -1,6 +1,6 @@
 // Problem 1: Complete the secondLargest function which takes in an array of numbers in input and return the second biggest number in the array. (without using sort)?
 
-<script type="text/javascript">
+
 function secondLargest(array) {
  var i; var sh;
 var fh=array[0];
@@ -23,10 +23,10 @@ else  {
 return sh;
 }
 
-</script>
+
 
 // Problem 2: Complete the calculateFrequency function that takes lowercase string as input and returns frequency of all english alphabet. (using only array, no in-built function)
-<script type="text/javascript">
+
 function calculateFrequency(string) {
   var obj = {};
   for(var i=0;i<string.length;i++) {
@@ -39,11 +39,9 @@ function calculateFrequency(string) {
   }
   return obj;
 }
-</script>
 // Problem 3: Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
 function flatten(unflatObject) {
   // Write your code here
-  <script type="text/javascript">
   var obj = {};
 	
 	for (var x in unflatObject){
@@ -61,11 +59,11 @@ function flatten(unflatObject) {
   console.log(obj);
 	return obj;
 }
-</script>
+
 
 // Problem 4: Complete the unflatten function that takes a JS Object, returns a JS Object in unflatten format
-<script type="text/javascript">
+
 function unflatten(flatObject) {
   // Write your code here
 }
-</script>
+
