213c2be7b047162cfe9e6ced7fa3a4c6c4dbbf5c
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-14 02:41:45 +0530

    asign5 mistakes removes with correct indentation

--- assignment5.js
+++ assignment5.js
@@ -1,21 +1,24 @@
 var fs = require('fs');
 
-var fileReader= function(fileName) {
-return new Promise( (resolve, reject) => {
- fs.readFile(fileName, 'utf8', function(error, data) {
-   console.log(data); 
-   console.log("============");
+var fileReader = function(fileName) {
+  return new Promise( (resolve, reject) => {
+    fs.readFile(fileName, 'utf8', function(error, data) {
+      console.log(data); 
+      console.log("============");
 
-   if(error) reject(fileName+'file not Done');
-     resolve(fileName+'fileDone');
-      });
+      if(error)
+        return reject(fileName + 'file not Done');
+      resolve(fileName +'fileDone');
+    });
   });
 };
 
 console.log("============");
 
-fileReader('file1').then(function() {
-  return fileReader('file2')
-  }).then(function() {
-  return fileReader('file3')
-});
+fileReader('file1')
+ .then(function() {
+    return fileReader('file2')
+  })
+ .then(function() {
+    return fileReader('file3')
+  });
