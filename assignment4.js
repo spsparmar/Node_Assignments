2a85c7db19cdfd50a28a00eee7d1d1c4aa912844
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-13 22:49:35 +0530

    Indentation done correctly

--- assignment4.js
+++ assignment4.js
@@ -1,16 +1,12 @@
 var fs = require('fs');
-var fileArray= ['file1','file2','file3','file4','file5'];
-
+var fileArray= ['file1', 'file2', 'file3', 'file4', 'file5'];
 console.log("============");	
-
-function myreadfile(index)	{
-	if(index==fileArray.length) return;
-
-	fs.readFile(fileArray[index], 'utf8', function(error,data)	{  
-		console.log(data);
-		console.log("============");
-		myreadfile(index+1);
-		
-	});
+function myReadfile(index)  {
+  if(index == fileArray.length) return;
+  fs.readFile(fileArray[index], 'utf8', function(error,data)  {  
+    console.log(data);
+    console.log("============");
+    myReadfile(index+1);
+  });
 }
-myreadfile(0);
+myReadfile(0);
