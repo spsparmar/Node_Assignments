c77df3fa5c264f90808f547cdb57e2b216c830fe
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-14 02:16:59 +0530

    asign5 mistakes removed

--- assignment5.js
+++ assignment5.js
@@ -1,14 +1,21 @@
 var fs = require('fs');
 
-var fileReader= function(file_name) {
-  fs.readFile(file_name, 'utf8', function(error, data) {
-    console.log(data);
-    console.log("============");
-    return new Promise( function(resolve, reject) {
-      resolve(file_name+'file_done');
-    });
+var fileReader= function(fileName) {
+return new Promise( (resolve, reject) => {
+ fs.readFile(fileName, 'utf8', function(error, data) {
+   console.log(data); 
+   console.log("============");
+
+   if(error) reject(fileName+'file not Done');
+     resolve(fileName+'fileDone');
+      });
   });
 };
 
 console.log("============");
-Promise.all([fileReader('file1'),fileReader('file2'),fileReader('file3')]);
+
+fileReader('file1').then(function() {
+  return fileReader('file2')
+  }).then(function() {
+  return fileReader('file3')
+});
