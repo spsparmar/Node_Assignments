231e5a1fb5db4e35dbfdf09d6a39caa2f12a67a1
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-13 23:32:48 +0530

    Newlines added and spaces corrected

--- assignment4.js
+++ assignment4.js
@@ -1,12 +1,17 @@
 var fs = require('fs');
+
 var fileArray= ['file1', 'file2', 'file3', 'file4', 'file5'];
-console.log("============");	
-function myReadfile(index)  {
-  if(index == fileArray.length) return;
-  fs.readFile(fileArray[index], 'utf8', function(error,data)  {
+
+console.log("============");
+
+function myReadfile(index) {
+  if(index == fileArray.length)
+  	return;
+  fs.readFile(fileArray[index], 'utf8', function(error, data) {
     console.log(data);
     console.log("============");
     myReadfile(index+1);
   });
 }
+
 myReadfile(0);
