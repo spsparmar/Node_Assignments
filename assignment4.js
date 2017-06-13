3e453f1d8ff87baaa56fc48d580c6a3cee6ae68c
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-13 22:51:48 +0530

    Indentation done correctly

--- assignment4.js
+++ assignment4.js
@@ -3,7 +3,7 @@ var fileArray= ['file1', 'file2', 'file3', 'file4', 'file5'];
 console.log("============");	
 function myReadfile(index)  {
   if(index == fileArray.length) return;
-  fs.readFile(fileArray[index], 'utf8', function(error,data)  {  
+  fs.readFile(fileArray[index], 'utf8', function(error,data)  {
     console.log(data);
     console.log("============");
     myReadfile(index+1);
