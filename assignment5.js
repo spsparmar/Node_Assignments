38d7465d79c751cb77600d5fda91adea1099531f
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-26 21:33:20 +0530

    Error handling done and (===) used

--- assignment5.js
+++ assignment5.js
@@ -3,12 +3,12 @@ var fs = require('fs');
 var fileReader = (fileName) => {
   return new Promise((resolve, reject) => {
     fs.readFile(fileName, 'utf8', (error, data) => {
-      console.log(data); 
-      console.log("============");
-
       if(error)
         return reject(error);
-      resolve(fileName + 'fileDone');
+
+      console.log(data); 
+      console.log("============");
+      resolve(fileName);
     });
   });
 };
@@ -16,11 +16,12 @@ var fileReader = (fileName) => {
 console.log("============");
 
 fileReader('file1')
- .then(() => {
+ .then((fileName) => {
     return fileReader('file2')
   })
- .then(() => {
+ .then((fileName) => {
     return fileReader('file3')
-  }).catch(() => {
-      console.log("error");
+  })
+ .catch((error) => {
+      console.log(error);
   });
