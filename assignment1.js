51618bd43bc492def50bbb050831aadcddfb2ce3
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-27 11:44:50 +0530

    Indentation improved, conditional operator is not used

--- assignment1.js
+++ assignment1.js
@@ -1,17 +1,15 @@
 // Problem 1: Complete the secondLargest function which takes in an array of numbers in input and return the second biggest number in the array. (without using sort)?
 function secondLargest(array) {
-  var max1, max2, i;
+  var max1 = array[0], max2 = array[1], index;
 
-  max1 = array[0], max2 = array[1];
-
-  for(i = 1; i < array.length; i++) {
-    if(array[i] > max1) {
+  for(index = 1; index < array.length; index++) {
+    if(array[index] > max1) {
       max2 = max1;
-      max1 = array[i];
+      max1 = array[index];
     }
     else
-      if(array[i] > max2 && array[i] != max1)
-        max2 = array[i];
+      if(array[index] > max2 && array[index] != max1)
+        max2 = array[index];
   }
   return max2;
 }
@@ -20,13 +18,17 @@ function secondLargest(array) {
 function calculateFrequency(string) {
   var obj = {};
   
-  for(var i=0;i<string.length;i++) {
-    var c = string.charAt(i);
+  for(var index = 0; index < string.length; index++) {
+    var char = string.charAt(index);
   
-    if( !(c >= 'a' && c <= 'z') )
-    continue;
-  
-    (obj[c]) ? obj[c]++ : obj[c]=1;
+    if(!(char >= 'a' && char <= 'z'))
+      continue;
+    
+    if(obj[char]) {
+      obj[char]++;
+    }
+    else
+      obj[char] = 1;
   }
   return obj;
 }
@@ -34,27 +36,40 @@ function calculateFrequency(string) {
 // Problem 3: Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
 function flatten(unflatObject) {
   var obj = {};
-  for(var x in unflatObject) {
-    if( (typeof unflatObject[x]) == 'object' ) {
-    var flatObj = flatten(unflatObject[x]);
+  
+  for(var key1 in unflatObject) {
+    if( (typeof unflatObject[key1]) == 'object' ) {
+      var flatObj = flatten(unflatObject[key1]);
     
-    for (var y in flatObj) {
-      obj[x + "." + y] = flatObj[y];
+      for(var key2 in flatObj) {
+        obj[key1 + "." + key2] = flatObj[key2];
+      }
     }
-  }
-  else
-      obj[x] = unflatObject[x];
+    else
+      obj[key1] = unflatObject[key1];
   }
   return obj;
 }
 
 // Problem 4: Complete the unflatten function that takes a JS Object, returns a JS Object in unflatten format
 function unflatten(flatObject) {
-  var result = {}
+  var result = {};
+
   for (var i in flatObject) {
-    var keys = i.split('.')
+    var keys = i.split('.');
+
     keys.reduce(function(array, current, index) {
-      return array[current] || (array[current] = isNaN(Number(keys[index])) ? (keys.length - 1 == index ? flatObject[i] : {}) : [])
+      if(!array[current]) {
+        array[current] = [];
+
+        if(isNaN(Number(keys[index]))) {
+          array[current] = {};
+
+          if(keys.length - 1 === index)
+          array[current] = flatObject[i];
+        }
+      }
+      return array[current];
     }, result)
   }
   return result
