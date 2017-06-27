a6b8af1cee3480ce8447a5171998877105cd7f55
Shiv Pratap Singh Parmar <spsingh@grepruby.com>
2017-06-27 19:18:37 +0530

    Error handling properly done

--- assignment4.js
+++ assignment4.js
@@ -1,23 +1,20 @@
 var fs = require('fs');
 var fileArray = ['file1', 'file2', 'file3', 'file4', 'file5'];
 
-console.log("============");
-
 function myReadfile(index) {
   if(index === fileArray.length)
     return;
 
   fs.readFile(fileArray[index], 'utf8', function(error, data) {
-    try {
-      console.log(data);
-      console.log("============");
+    if(error) {
+      console.log(error + ":File" + (index + 1) );
     }
-    catch(error) {
-      console.log(error + "unable to read file");
-    }
-    finally {
-      myReadfile(index + 1);
-    }
+
+    console.log(data);
+    console.log("============");
+    myReadfile(index + 1);
   });
 }
+
+console.log("============");
 myReadfile(0);
